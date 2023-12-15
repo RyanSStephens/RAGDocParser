d141a7b1ff4efa0cd3184ec4df7abf276395a431
Riboflavin <me@ryanstephens.work>
2023-12-15 14:20:00 -0500

    Add basic package structure and configuration

--- ragdocparser/__init__.py
+++ ragdocparser/__init__.py
@@ -1,23 +1,13 @@
 """
-RAGDocParser - Document parser optimized for RAG systems.
-
-A comprehensive document processing library that handles multiple formats,
-performs OCR on images, and integrates with vector databases for optimal
-RAG (Retrieval-Augmented Generation) performance.
+RAG Document Parser - A comprehensive document processing system for RAG applications.
 """
 
-__version__ = "1.5.0"
+__version__ = "0.1.0"
 __author__ = "Ryan Stephens"
 __email__ = "ryan@example.com"
 
 from .parser import DocumentParser
 from .chunker import TextChunker
-from .vectordb import VectorDBManager
 from .config import Config
 
-__all__ = [
-    "DocumentParser",
-    "TextChunker", 
-    "VectorDBManager",
-    "Config"
-] 
+__all__ = ["DocumentParser", "TextChunker", "Config"]
--- ragdocparser/config.py
+++ ragdocparser/config.py
@@ -1,116 +1,36 @@
 """
-Configuration management for RAGDocParser.
+Configuration management for RAG Document Parser.
 """
 
 import os
-from typing import Dict, Any, Optional
-from pathlib import Path
-import json
-from dotenv import load_dotenv
+from typing import Optional
+from pydantic import BaseSettings, Field
 
-# Load environment variables
-load_dotenv()
 
-class Config:
-    """Configuration management for RAG document parser."""
+class Config(BaseSettings):
+    """Application configuration."""
     
-    def __init__(self, config_path: Optional[str] = None):
-        """Initialize configuration.
-        
-        Args:
-            config_path: Path to configuration file
-        """
-        self.config_path = config_path
-        self._config = self._load_default_config()
-        
-        if config_path and os.path.exists(config_path):
-            self._load_config_file()
-            
-    def _load_default_config(self) -> Dict[str, Any]:
-        """Load default configuration values."""
-        return {
-            "chunking": {
-                "chunk_size": 1000,
-                "chunk_overlap": 200,
-                "min_chunk_size": 100
-            },
-            "vector_db": {
-                "provider": "chromadb",
-                "collection_name": "ragdocparser",
-                "embedding_model": "sentence-transformers/all-MiniLM-L6-v2"
-            },
-            "llm": {
-                "openai_api_key": os.getenv("OPENAI_API_KEY"),
-                "anthropic_api_key": os.getenv("ANTHROPIC_API_KEY"),
-                "cohere_api_key": os.getenv("COHERE_API_KEY"),
-                "default_model": "openai"
-            },
-            "ocr": {
-                "tesseract_path": None,
-                "language": "eng",
-                "dpi": 300
-            },
-            "parsing": {
-                "supported_formats": [".pdf", ".docx", ".txt", ".html", ".md"],
-                "max_file_size_mb": 50,
-                "timeout_seconds": 300
-            }
-        }
+    # API Keys
+    openai_api_key: Optional[str] = Field(None, env="OPENAI_API_KEY")
+    anthropic_api_key: Optional[str] = Field(None, env="ANTHROPIC_API_KEY")
     
-    def _load_config_file(self):
-        """Load configuration from file."""
-        try:
-            with open(self.config_path, 'r') as f:
-                file_config = json.load(f)
-                self._merge_config(file_config)
-        except Exception as e:
-            print(f"Warning: Could not load config file {self.config_path}: {e}")
+    # Vector Database
+    chroma_persist_directory: str = Field("./chroma_db", env="CHROMA_PERSIST_DIRECTORY")
     
-    def _merge_config(self, new_config: Dict[str, Any]):
-        """Merge new configuration with existing."""
-        for key, value in new_config.items():
-            if key in self._config and isinstance(self._config[key], dict):
-                self._config[key].update(value)
-            else:
-                self._config[key] = value
+    # OCR Settings
+    tesseract_path: Optional[str] = Field(None, env="TESSERACT_PATH")
     
-    def get(self, key: str, default: Any = None) -> Any:
-        """Get configuration value by key path (e.g., 'chunking.chunk_size')."""
-        keys = key.split('.')
-        value = self._config
-        
-        for k in keys:
-            if isinstance(value, dict) and k in value:
-                value = value[k]
-            else:
-                return default
-                
-        return value
+    # Chunking
+    default_chunk_size: int = Field(1000, env="DEFAULT_CHUNK_SIZE")
+    default_chunk_overlap: int = Field(200, env="DEFAULT_CHUNK_OVERLAP")
     
-    def set(self, key: str, value: Any):
-        """Set configuration value by key path."""
-        keys = key.split('.')
-        config = self._config
-        
-        for k in keys[:-1]:
-            if k not in config:
-                config[k] = {}
-            config = config[k]
-            
-        config[keys[-1]] = value
+    # Logging
+    log_level: str = Field("INFO", env="LOG_LEVEL")
     
-    def save(self, path: Optional[str] = None):
-        """Save configuration to file."""
-        save_path = path or self.config_path
-        if not save_path:
-            raise ValueError("No path specified for saving configuration")
-            
-        os.makedirs(os.path.dirname(save_path), exist_ok=True)
-        
-        with open(save_path, 'w') as f:
-            json.dump(self._config, f, indent=2)
-    
-    @property
-    def config(self) -> Dict[str, Any]:
-        """Get full configuration dictionary."""
-        return self._config.copy() 
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+# Global config instance
+config = Config()
