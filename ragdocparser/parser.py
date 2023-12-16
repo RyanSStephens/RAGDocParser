80d11cf6e600807b6d6d2c156560311a869357c6
Riboflavin <me@ryanstephens.work>
2023-12-16 10:15:00 -0500

    Implement basic document parser with PDF support

--- ragdocparser/parser.py
+++ ragdocparser/parser.py
@@ -1,240 +1,151 @@
 """
-Main document parser module with multi-format support and OCR.
+Document parser for various file formats.
 """
 
 import os
-import mimetypes
-from pathlib import Path
-from typing import List, Dict, Any, Optional, Union
 import logging
-from dataclasses import dataclass
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import time
-import hashlib
+from pathlib import Path
+from typing import Dict, List, Optional, Union
+from abc import ABC, abstractmethod
 
-# Document processing imports
-import PyPDF2
-from docx import Document as DocxDocument
-from bs4 import BeautifulSoup
-import requests
-from PIL import Image
-import pytesseract
+try:
+    import PyPDF2
+    PDF_AVAILABLE = True
+except ImportError:
+    PDF_AVAILABLE = False
 
-# Internal imports
-from .config import Config
-from .chunker import TextChunker, TextChunk
-from .vectordb import VectorDBManager
+try:
+    from docx import Document as DocxDocument
+    DOCX_AVAILABLE = True
+except ImportError:
+    DOCX_AVAILABLE = False
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
-@dataclass
-class DocumentInfo:
-    """Information about a parsed document."""
-    filename: str
-    filepath: str
-    content: str
-    metadata: Dict[str, Any]
-    parse_time: float
-    file_size: int
-    content_hash: str
 
-class DocumentParser:
-    """Main document parser with multi-format support."""
+class BaseParser(ABC):
+    """Base class for document parsers."""
     
-    def __init__(self, config: Optional[Config] = None):
-        """Initialize document parser."""
-        self.config = config or Config()
-        self.chunker = TextChunker(
-            chunk_size=self.config.get("chunking.chunk_size", 1000),
-            chunk_overlap=self.config.get("chunking.chunk_overlap", 200),
-            min_chunk_size=self.config.get("chunking.min_chunk_size", 100)
-        )
-        self.vectordb = VectorDBManager(self.config)
-        
-        # Supported file extensions
-        self.supported_formats = self.config.get(
-            "parsing.supported_formats", 
-            [".pdf", ".docx", ".txt", ".html", ".md"]
-        )
-        
-        # OCR configuration
-        self.ocr_enabled = True
-        tesseract_path = self.config.get("ocr.tesseract_path")
-        if tesseract_path:
-            pytesseract.pytesseract.tesseract_cmd = tesseract_path
+    @abstractmethod
+    def parse(self, file_path: Union[str, Path]) -> Dict[str, any]:
+        """Parse a document and return extracted content."""
+        pass
+
+
+class PDFParser(BaseParser):
+    """Parser for PDF documents."""
     
-    def _extract_text_from_pdf(self, filepath: str) -> str:
-        """Extract text from PDF file."""
+    def parse(self, file_path: Union[str, Path]) -> Dict[str, any]:
+        """Parse PDF document."""
+        if not PDF_AVAILABLE:
+            raise ImportError("PyPDF2 is required for PDF parsing. Install with: pip install PyPDF2")
+        
+        file_path = Path(file_path)
+        if not file_path.exists():
+            raise FileNotFoundError(f"File not found: {file_path}")
+        
+        text_content = []
+        metadata = {}
+        
         try:
-            with open(filepath, 'rb') as file:
+            with open(file_path, 'rb') as file:
                 pdf_reader = PyPDF2.PdfReader(file)
-                text = ""
+                metadata.update({
+                    'title': pdf_reader.metadata.get('/Title', ''),
+                    'author': pdf_reader.metadata.get('/Author', ''),
+                    'subject': pdf_reader.metadata.get('/Subject', ''),
+                    'creator': pdf_reader.metadata.get('/Creator', ''),
+                    'producer': pdf_reader.metadata.get('/Producer', ''),
+                    'creation_date': pdf_reader.metadata.get('/CreationDate', ''),
+                    'modification_date': pdf_reader.metadata.get('/ModDate', ''),
+                    'pages': len(pdf_reader.pages)
+                })
                 
                 for page_num, page in enumerate(pdf_reader.pages):
                     try:
                         page_text = page.extract_text()
                         if page_text.strip():
-                            text += f"\n\n--- Page {page_num + 1} ---\n{page_text}"
+                            text_content.append({
+                                'page': page_num + 1,
+                                'content': page_text.strip()
+                            })
                     except Exception as e:
                         logger.warning(f"Error extracting text from page {page_num + 1}: {e}")
                         
-                return text.strip()
         except Exception as e:
-            logger.error(f"Error reading PDF {filepath}: {e}")
-            return ""
+            logger.error(f"Error parsing PDF {file_path}: {e}")
+            raise
+        
+        return {
+            'content': text_content,
+            'metadata': metadata,
+            'file_path': str(file_path),
+            'file_type': 'pdf'
+        }
+
+
+class TXTParser(BaseParser):
+    """Parser for plain text documents."""
     
-    def _extract_text_from_docx(self, filepath: str) -> str:
-        """Extract text from DOCX file."""
+    def parse(self, file_path: Union[str, Path]) -> Dict[str, any]:
+        """Parse text document."""
+        file_path = Path(file_path)
+        if not file_path.exists():
+            raise FileNotFoundError(f"File not found: {file_path}")
+        
         try:
-            doc = DocxDocument(filepath)
-            text = ""
-            
-            for paragraph in doc.paragraphs:
-                if paragraph.text.strip():
-                    text += paragraph.text + "\n"
-            
-            return text.strip()
-        except Exception as e:
-            logger.error(f"Error reading DOCX {filepath}: {e}")
-            return ""
-    
-    def _extract_text_from_html(self, filepath: str) -> str:
-        """Extract text from HTML file."""
-        try:
-            with open(filepath, 'r', encoding='utf-8', errors='ignore') as file:
-                soup = BeautifulSoup(file.read(), 'html.parser')
-                
-                # Remove script and style elements
-                for script in soup(["script", "style"]):
-                    script.decompose()
-                
-                # Get text and clean it up
-                text = soup.get_text()
-                lines = (line.strip() for line in text.splitlines())
-                chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-                text = ' '.join(chunk for chunk in chunks if chunk)
-                
-                return text
-        except Exception as e:
-            logger.error(f"Error reading HTML {filepath}: {e}")
-            return ""
-    
-    def _extract_text_from_txt(self, filepath: str) -> str:
-        """Extract text from plain text file."""
-        try:
-            with open(filepath, 'r', encoding='utf-8', errors='ignore') as file:
-                return file.read()
-        except Exception as e:
-            logger.error(f"Error reading text file {filepath}: {e}")
-            return ""
-    
-    def parse_file(self, filepath: str) -> Optional[DocumentInfo]:
-        """Parse a single file and extract text."""
-        start_time = time.time()
+            with open(file_path, 'r', encoding='utf-8') as file:
+                content = file.read()
+        except UnicodeDecodeError:
+            # Try different encodings
+            for encoding in ['latin-1', 'cp1252', 'iso-8859-1']:
+                try:
+                    with open(file_path, 'r', encoding=encoding) as file:
+                        content = file.read()
+                    break
+                except UnicodeDecodeError:
+                    continue
+            else:
+                raise ValueError(f"Unable to decode file {file_path}")
         
-        if not os.path.exists(filepath):
-            logger.error(f"File not found: {filepath}")
-            return None
-        
-        path = Path(filepath)
-        extension = path.suffix.lower()
-        
-        # Extract text based on file type
-        content = ""
-        if extension == ".pdf":
-            content = self._extract_text_from_pdf(filepath)
-        elif extension == ".docx":
-            content = self._extract_text_from_docx(filepath)
-        elif extension in [".html", ".htm"]:
-            content = self._extract_text_from_html(filepath)
-        elif extension in [".txt", ".md"]:
-            content = self._extract_text_from_txt(filepath)
-        else:
-            content = self._extract_text_from_txt(filepath)
-        
-        if not content.strip():
-            logger.warning(f"No content extracted from {filepath}")
-            return None
-        
-        # Get metadata
         metadata = {
-            "filename": path.name,
-            "file_extension": extension,
-            "file_size": path.stat().st_size,
-            "directory": str(path.parent)
+            'file_size': file_path.stat().st_size,
+            'creation_date': file_path.stat().st_ctime,
+            'modification_date': file_path.stat().st_mtime,
         }
         
-        parse_time = time.time() - start_time
-        content_hash = hashlib.sha256(content.encode('utf-8')).hexdigest()[:16]
-        
-        return DocumentInfo(
-            filename=path.name,
-            filepath=str(path.absolute()),
-            content=content,
-            metadata=metadata,
-            parse_time=parse_time,
-            file_size=path.stat().st_size,
-            content_hash=content_hash
-        )
+        return {
+            'content': [{'page': 1, 'content': content}],
+            'metadata': metadata,
+            'file_path': str(file_path),
+            'file_type': 'txt'
+        }
+
+
+class DocumentParser:
+    """Main document parser that handles multiple file formats."""
     
-    def parse_directory(self, directory: str, recursive: bool = True, max_workers: int = 4) -> List[DocumentInfo]:
-        """Parse all supported files in a directory with parallel processing."""
-        if not os.path.exists(directory):
-            logger.error(f"Directory not found: {directory}")
-            return []
+    def __init__(self):
+        self.parsers = {
+            '.pdf': PDFParser(),
+            '.txt': TXTParser(),
+        }
         
-        # Collect all files to parse
-        files_to_parse = []
-        path = Path(directory)
-        
-        if recursive:
-            for ext in self.supported_formats:
-                files_to_parse.extend(path.rglob(f"*{ext}"))
-        else:
-            for file in path.iterdir():
-                if file.is_file() and file.suffix.lower() in self.supported_formats:
-                    files_to_parse.append(file)
-        
-        logger.info(f"Found {len(files_to_parse)} files to parse")
-        
-        # Parse files in parallel for better performance
-        documents = []
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            future_to_file = {
-                executor.submit(self.parse_file, str(file)): file 
-                for file in files_to_parse
-            }
-            
-            for future in as_completed(future_to_file):
-                file = future_to_file[future]
-                try:
-                    doc_info = future.result()
-                    if doc_info:
-                        documents.append(doc_info)
-                        logger.info(f"Parsed {doc_info.filename}")
-                except Exception as e:
-                    logger.error(f"Error parsing {file}: {e}")
-        
-        return documents
+        if DOCX_AVAILABLE:
+            self.parsers['.docx'] = DocxParser()
     
-    def save_to_vector_db(self, documents: List[DocumentInfo], collection_name: str = None):
-        """Process documents and save to vector database."""
-        all_chunks = []
+    def parse(self, file_path: Union[str, Path]) -> Dict[str, any]:
+        """Parse a document based on its file extension."""
+        file_path = Path(file_path)
+        extension = file_path.suffix.lower()
         
-        for doc in documents:
-            doc_metadata = doc.metadata.copy()
-            doc_metadata.update({
-                "content_hash": doc.content_hash,
-                "source": doc.filepath
-            })
-            
-            chunks = self.chunker.chunk_text(doc.content, doc_metadata)
-            all_chunks.extend(chunks)
+        if extension not in self.parsers:
+            raise ValueError(f"Unsupported file format: {extension}")
         
-        if all_chunks:
-            self.chunker.generate_embeddings(all_chunks)
-            self.vectordb.add_documents(all_chunks, collection_name)
-        
-        return len(all_chunks) 
+        parser = self.parsers[extension]
+        return parser.parse(file_path)
+    
+    def supported_formats(self) -> List[str]:
+        """Return list of supported file formats."""
+        return list(self.parsers.keys())
