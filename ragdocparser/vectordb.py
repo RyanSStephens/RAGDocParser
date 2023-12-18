e200eaf9778922712f0fe4b73502658c1b96bcd5
Riboflavin <me@ryanstephens.work>
2023-12-18 14:30:00 -0500

    Add vector database integration with ChromaDB

--- ragdocparser/vectordb.py
+++ ragdocparser/vectordb.py
@@ -1,13 +1,11 @@
 """
-Vector database integration for RAG systems.
+Vector database integration for storing and retrieving document embeddings.
 """
 
-import os
+import logging
 import uuid
-from typing import List, Dict, Any, Optional, Tuple
-from abc import ABC, abstractmethod
-import numpy as np
-from dataclasses import asdict
+from typing import List, Dict, Any, Optional, Union
+from pathlib import Path
 
 try:
     import chromadb
@@ -16,252 +14,287 @@ try:
 except ImportError:
     CHROMADB_AVAILABLE = False
 
-from .chunker import TextChunk
-from .config import Config
+try:
+    import openai
+    OPENAI_AVAILABLE = True
+except ImportError:
+    OPENAI_AVAILABLE = False
 
-class VectorDBInterface(ABC):
-    """Abstract interface for vector databases."""
-    
-    @abstractmethod
-    def create_collection(self, name: str, **kwargs):
-        """Create a new collection."""
-        pass
-    
-    @abstractmethod
-    def add_chunks(self, chunks: List[TextChunk], collection_name: str = None):
-        """Add text chunks to the database."""
-        pass
-    
-    @abstractmethod
-    def search(self, query: str, k: int = 5, collection_name: str = None) -> List[Dict[str, Any]]:
-        """Search for similar chunks."""
-        pass
-    
-    @abstractmethod
-    def delete_collection(self, name: str):
-        """Delete a collection."""
-        pass
+from .chunker import Chunk
+from .config import config
 
-class ChromaDBManager(VectorDBInterface):
-    """ChromaDB implementation for vector storage."""
+logger = logging.getLogger(__name__)
+
+
+class EmbeddingProvider:
+    """Base class for embedding providers."""
     
-    def __init__(self, config: Config):
-        """Initialize ChromaDB manager.
+    def embed_texts(self, texts: List[str]) -> List[List[float]]:
+        """Generate embeddings for a list of texts."""
+        raise NotImplementedError
+    
+    def embed_query(self, query: str) -> List[float]:
+        """Generate embedding for a single query."""
+        return self.embed_texts([query])[0]
+
+
+class OpenAIEmbeddingProvider(EmbeddingProvider):
+    """OpenAI embedding provider."""
+    
+    def __init__(self, api_key: Optional[str] = None, model: str = "text-embedding-ada-002"):
+        if not OPENAI_AVAILABLE:
+            raise ImportError("openai package is required for OpenAI embeddings")
         
-        Args:
-            config: Configuration object
-        """
+        self.api_key = api_key or config.openai_api_key
+        if not self.api_key:
+            raise ValueError("OpenAI API key is required")
+        
+        openai.api_key = self.api_key
+        self.model = model
+    
+    def embed_texts(self, texts: List[str]) -> List[List[float]]:
+        """Generate embeddings using OpenAI API."""
+        try:
+            response = openai.Embedding.create(
+                input=texts,
+                model=self.model
+            )
+            return [item['embedding'] for item in response['data']]
+        except Exception as e:
+            logger.error(f"Error generating OpenAI embeddings: {e}")
+            raise
+
+
+class VectorDatabase:
+    """Vector database interface using ChromaDB."""
+    
+    def __init__(self, 
+                 collection_name: str = "documents",
+                 persist_directory: Optional[str] = None,
+                 embedding_provider: Optional[EmbeddingProvider] = None):
+        
         if not CHROMADB_AVAILABLE:
-            raise ImportError("ChromaDB not available. Install with: pip install chromadb")
-            
-        self.config = config
-        self.db_path = config.get("vector_db.db_path", "./chroma_db")
-        self.embedding_model = config.get("vector_db.embedding_model", "sentence-transformers/all-MiniLM-L6-v2")
-        self.default_collection = config.get("vector_db.collection_name", "ragdocparser")
+            raise ImportError("chromadb package is required for vector database functionality")
+        
+        self.collection_name = collection_name
+        self.persist_directory = persist_directory or config.chroma_persist_directory
         
         # Initialize ChromaDB client
         self.client = chromadb.PersistentClient(
-            path=self.db_path,
+            path=self.persist_directory,
             settings=Settings(
                 anonymized_telemetry=False,
                 allow_reset=True
             )
         )
         
-        self._collections = {}
-    
-    def create_collection(self, name: str, **kwargs):
-        """Create a new ChromaDB collection."""
-        try:
-            collection = self.client.create_collection(
-                name=name,
-                metadata={"hnsw:space": "cosine"}
-            )
-            self._collections[name] = collection
-            return collection
-        except Exception as e:
-            # Collection might already exist
-            collection = self.client.get_collection(name)
-            self._collections[name] = collection
-            return collection
-    
-    def get_collection(self, name: str = None):
-        """Get or create collection."""
-        collection_name = name or self.default_collection
-        
-        if collection_name not in self._collections:
+        # Initialize embedding provider
+        self.embedding_provider = embedding_provider
+        if self.embedding_provider is None and OPENAI_AVAILABLE and config.openai_api_key:
             try:
-                self._collections[collection_name] = self.client.get_collection(collection_name)
-            except:
-                self._collections[collection_name] = self.create_collection(collection_name)
+                self.embedding_provider = OpenAIEmbeddingProvider()
+                logger.info("Using OpenAI embeddings")
+            except Exception as e:
+                logger.warning(f"Failed to initialize OpenAI embeddings: {e}")
         
-        return self._collections[collection_name]
+        # Get or create collection
+        try:
+            self.collection = self.client.get_collection(collection_name)
+            logger.info(f"Loaded existing collection: {collection_name}")
+        except ValueError:
+            # Collection doesn't exist, create it
+            if self.embedding_provider:
+                self.collection = self.client.create_collection(
+                    name=collection_name,
+                    metadata={"description": "Document chunks with embeddings"}
+                )
+            else:
+                # Use ChromaDB's default embedding function
+                self.collection = self.client.create_collection(
+                    name=collection_name,
+                    metadata={"description": "Document chunks with default embeddings"}
+                )
+            logger.info(f"Created new collection: {collection_name}")
     
-    def add_chunks(self, chunks: List[TextChunk], collection_name: str = None):
-        """Add text chunks to ChromaDB."""
+    def add_chunks(self, chunks: List[Chunk]) -> None:
+        """Add chunks to the vector database."""
         if not chunks:
+            logger.warning("No chunks to add")
             return
         
-        collection = self.get_collection(collection_name)
-        
         # Prepare data for ChromaDB
-        ids = []
         documents = []
-        embeddings = []
         metadatas = []
+        ids = []
+        embeddings = []
         
         for chunk in chunks:
-            chunk_id = str(uuid.uuid4())
-            ids.append(chunk_id)
+            # Generate unique ID if not present
+            chunk_id = chunk.chunk_id or str(uuid.uuid4())
+            
             documents.append(chunk.content)
-            
-            # Use existing embedding or generate placeholder
-            if chunk.embedding:
-                embeddings.append(chunk.embedding)
-            else:
-                # Generate a placeholder embedding if none exists
-                # In practice, embeddings should be generated before storing
-                                 # Skip chunks without embeddings in production
-                 continue
-            
-            # Prepare metadata
-            metadata = chunk.metadata.copy()
-            metadata.update({
-                "token_count": chunk.token_count,
-                "start_index": chunk.start_index,
-                "end_index": chunk.end_index
-            })
-            metadatas.append(metadata)
+            metadatas.append(chunk.metadata or {})
+            ids.append(chunk_id)
         
-        # Add to collection
-        collection.add(
-            ids=ids,
-            documents=documents,
-            embeddings=embeddings,
-            metadatas=metadatas
-        )
+        try:
+            # Generate embeddings if provider is available
+            if self.embedding_provider:
+                logger.info(f"Generating embeddings for {len(documents)} chunks...")
+                embeddings = self.embedding_provider.embed_texts(documents)
+                
+                self.collection.add(
+                    documents=documents,
+                    metadatas=metadatas,
+                    ids=ids,
+                    embeddings=embeddings
+                )
+            else:
+                # Let ChromaDB handle embeddings
+                self.collection.add(
+                    documents=documents,
+                    metadatas=metadatas,
+                    ids=ids
+                )
+            
+            logger.info(f"Successfully added {len(chunks)} chunks to vector database")
+            
+        except Exception as e:
+            logger.error(f"Error adding chunks to vector database: {e}")
+            raise
     
     def search(self, 
                query: str, 
-               k: int = 5, 
-               collection_name: str = None,
-               embedding: List[float] = None) -> List[Dict[str, Any]]:
-        """Search for similar chunks in ChromaDB."""
-        collection = self.get_collection(collection_name)
+               n_results: int = 5,
+               where: Optional[Dict[str, Any]] = None,
+               where_document: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
+        """Search for similar chunks."""
+        try:
+            # Generate query embedding if provider is available
+            if self.embedding_provider:
+                query_embedding = self.embedding_provider.embed_query(query)
+                results = self.collection.query(
+                    query_embeddings=[query_embedding],
+                    n_results=n_results,
+                    where=where,
+                    where_document=where_document
+                )
+            else:
+                # Use ChromaDB's default embedding
+                results = self.collection.query(
+                    query_texts=[query],
+                    n_results=n_results,
+                    where=where,
+                    where_document=where_document
+                )
+            
+            return results
+            
+        except Exception as e:
+            logger.error(f"Error searching vector database: {e}")
+            raise
+    
+    def get_collection_info(self) -> Dict[str, Any]:
+        """Get information about the collection."""
+        try:
+            count = self.collection.count()
+            return {
+                'name': self.collection_name,
+                'count': count,
+                'persist_directory': self.persist_directory,
+                'embedding_provider': type(self.embedding_provider).__name__ if self.embedding_provider else 'ChromaDB Default'
+            }
+        except Exception as e:
+            logger.error(f"Error getting collection info: {e}")
+            return {}
+    
+    def delete_collection(self) -> None:
+        """Delete the entire collection."""
+        try:
+            self.client.delete_collection(self.collection_name)
+            logger.info(f"Deleted collection: {self.collection_name}")
+        except Exception as e:
+            logger.error(f"Error deleting collection: {e}")
+            raise
+    
+    def reset_collection(self) -> None:
+        """Reset the collection (delete all data)."""
+        try:
+            self.client.delete_collection(self.collection_name)
+            self.collection = self.client.create_collection(
+                name=self.collection_name,
+                metadata={"description": "Document chunks with embeddings"}
+            )
+            logger.info(f"Reset collection: {self.collection_name}")
+        except Exception as e:
+            logger.error(f"Error resetting collection: {e}")
+            raise
+
+
+class DocumentVectorStore:
+    """High-level interface for document vector storage."""
+    
+    def __init__(self, 
+                 collection_name: str = "documents",
+                 persist_directory: Optional[str] = None,
+                 embedding_provider: Optional[EmbeddingProvider] = None):
         
-        if embedding:
-            # Search using provided embedding
-            results = collection.query(
-                query_embeddings=[embedding],
-                n_results=k
-            )
-        else:
-            # Search using query text (ChromaDB will generate embedding)
-            results = collection.query(
-                query_texts=[query],
-                n_results=k
-            )
+        self.vector_db = VectorDatabase(
+            collection_name=collection_name,
+            persist_directory=persist_directory,
+            embedding_provider=embedding_provider
+        )
+    
+    def add_document(self, document: Dict[str, Any], chunker) -> List[Chunk]:
+        """Add a document to the vector store."""
+        # Chunk the document
+        chunks = chunker.chunk_document(document)
+        
+        if not chunks:
+            logger.warning(f"No chunks generated for document: {document.get('file_path', 'unknown')}")
+            return []
+        
+        # Add chunks to vector database
+        self.vector_db.add_chunks(chunks)
+        
+        logger.info(f"Added document with {len(chunks)} chunks: {document.get('file_path', 'unknown')}")
+        return chunks
+    
+    def search_documents(self, 
+                        query: str, 
+                        n_results: int = 5,
+                        file_type: Optional[str] = None,
+                        file_path: Optional[str] = None) -> List[Dict[str, Any]]:
+        """Search for relevant document chunks."""
+        where_filter = {}
+        
+        if file_type:
+            where_filter['file_type'] = file_type
+        
+        if file_path:
+            where_filter['file_path'] = file_path
+        
+        where = where_filter if where_filter else None
+        
+        results = self.vector_db.search(
+            query=query,
+            n_results=n_results,
+            where=where
+        )
         
         # Format results
         formatted_results = []
-        if results['documents'] and results['documents'][0]:
-            for i in range(len(results['documents'][0])):
+        if results.get('documents'):
+            for i, doc in enumerate(results['documents'][0]):
                 result = {
-                    'id': results['ids'][0][i],
-                    'content': results['documents'][0][i],
-                    'metadata': results['metadatas'][0][i],
-                    'distance': results['distances'][0][i] if 'distances' in results else None
+                    'content': doc,
+                    'metadata': results['metadatas'][0][i] if results.get('metadatas') else {},
+                    'distance': results['distances'][0][i] if results.get('distances') else None,
+                    'id': results['ids'][0][i] if results.get('ids') else None,
                 }
                 formatted_results.append(result)
         
         return formatted_results
     
-    def delete_collection(self, name: str):
-        """Delete a ChromaDB collection."""
-        try:
-            self.client.delete_collection(name)
-            if name in self._collections:
-                del self._collections[name]
-        except Exception as e:
-            print(f"Error deleting collection {name}: {e}")
-    
-    def get_collection_stats(self, collection_name: str = None) -> Dict[str, Any]:
-        """Get statistics about a collection."""
-        collection = self.get_collection(collection_name)
-        
-        try:
-            count = collection.count()
-            return {
-                "document_count": count,
-                "collection_name": collection_name or self.default_collection
-            }
-        except Exception as e:
-            return {"error": str(e)}
-
-class VectorDBManager:
-    """Main vector database manager that can work with different backends."""
-    
-    def __init__(self, config: Config):
-        """Initialize vector DB manager.
-        
-        Args:
-            config: Configuration object
-        """
-        self.config = config
-        provider = config.get("vector_db.provider", "chromadb").lower()
-        
-        if provider == "chromadb":
-            self.db = ChromaDBManager(config)
-        else:
-            raise ValueError(f"Unsupported vector DB provider: {provider}")
-    
-    def add_documents(self, 
-                     chunks: List[TextChunk], 
-                     collection_name: str = None,
-                     generate_embeddings: bool = True):
-        """Add documents to vector database.
-        
-        Args:
-            chunks: List of text chunks to add
-            collection_name: Name of collection to add to
-            generate_embeddings: Whether to generate embeddings if missing
-        """
-        if generate_embeddings:
-            # Check if any chunks are missing embeddings
-            missing_embeddings = [c for c in chunks if c.embedding is None]
-            if missing_embeddings:
-                from .chunker import TextChunker
-                chunker = TextChunker()
-                chunker.generate_embeddings(missing_embeddings, self.config.get("vector_db.embedding_model"))
-        
-        self.db.add_chunks(chunks, collection_name)
-    
-    def search_similar(self, 
-                      query: str, 
-                      k: int = 5, 
-                      collection_name: str = None) -> List[Dict[str, Any]]:
-        """Search for similar documents.
-        
-        Args:
-            query: Search query
-            k: Number of results to return
-            collection_name: Collection to search in
-            
-        Returns:
-            List of similar documents with metadata
-        """
-        return self.db.search(query, k, collection_name)
-    
-    def create_collection(self, name: str, **kwargs):
-        """Create a new collection."""
-        return self.db.create_collection(name, **kwargs)
-    
-    def delete_collection(self, name: str):
-        """Delete a collection."""
-        self.db.delete_collection(name)
-    
-    def get_stats(self, collection_name: str = None) -> Dict[str, Any]:
-        """Get collection statistics."""
-        if hasattr(self.db, 'get_collection_stats'):
-            return self.db.get_collection_stats(collection_name)
-        return {"error": "Stats not available for this provider"} 
+    def get_stats(self) -> Dict[str, Any]:
+        """Get vector store statistics."""
+        return self.vector_db.get_collection_info()
